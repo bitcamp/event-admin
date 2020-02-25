3a37c56b14e7b3a76e8ea831855271e1be8212a3
Evan McIntire <mcintire.evan@gmail.com>
2020-02-24 19:27:25 -0500

    Scaffold some endpoints

--- server/server.py
+++ server/server.py
@@ -1,6 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+import hashlib
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+def get_events():
+    return []
+
+# Returns JSON with events.
+# Mirrors https://my.bit.camp/events.json
+@app.route('/events')
+def get_events():
+    return jsonify([])
+
+# Returns hashed string of data from /events
+@app.route('/events/hash')
+def get_events_hash():
+    return jsonify([])
+
+# Takes an event ID and an access token, favorites an event
+@app.route('/events/favorite', methods=['POST'])
+def post_events_favorite()
+    return jsonify([])
+
+# Takes an event ID and an access token, unfavorites an event
+@app.route('/events/unfavorite', methods=['POST'])
+def post_events_favorite()
+    return jsonify([])
+
+# Gets a count of all favorites
+@app.route('/events/favorite_count')
+def post_events_favorite()
+    return jsonify([])
+
+# Given an access token,
+@app.route('/events/my_favorites')
+def post_events_favorite()
+    return jsonify([])
