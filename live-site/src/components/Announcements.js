e1e889fae5ef916f0065ffb1d720566dec79c314
Edwin Yu <theedwinyu888@gmail.com>
2020-05-08 20:40:25 -0400

    add live site to event-admin repo

---
+++ live-site/src/components/Announcements.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { List, Avatar, Card, Divider } from 'antd';
+import fire from '../assets/fire.gif'
+import moment from 'moment';
+
+import announcementLogo from '../assets/announcements.svg'
+
+const { Meta } = Card;
+
+class Announcements extends Component{
+
+    constructor(){
+
+        super();
+        this.state = {
+            announcements: []
+        }
+        
+    }
+
+    componentDidMount(){
+
+        this.setState({
+            announcements: [
+                {
+                  title: moment(new Date("3/1/2020")).fromNow(),
+                  description: 'Red Wristband lunch time!'
+                },
+                {
+                  title: moment(new Date("2/29/2020")).fromNow(),
+                  description: 'Orange Wristband Lunch time!'
+                },
+                {
+                  title: moment(new Date("2/25/2020")).fromNow(),
+                  description: 'Blue Wristband Lunch time!'
+                },
+                {
+                  title: moment(new Date("1/1/2020")).fromNow(),
+                  description: 'White Wristband Lunch time!'
+                },
+            ]
+        })
+
+    }
+
+    render(){
+
+        return(
+
+            <Card bordered={true} style={{ backgroundColor:'white', borderRadius:'15px', marginLeft: '5%', marginRight:'10%',marginTop:'10%', height:'90%'}}>
+                <Meta
+                    title={<div><img src={announcementLogo} style={{height:'15%',width:'15%', float:'left'}}/><h1 style={{color:"#1A2E33", paddingLeft:'25%',fontFamily: 'Aleo'}}><b>Annoucements</b></h1></div>}
+                    style={{color:'#1A2E33'}}
+                />
+                {/* <Divider style={{marginBottom:'5%',marginTop:'1%'}}/> */}
+                <Divider/>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={this.state.announcements}
+                    renderItem={item => (
+                    <List.Item>
+                        <List.Item.Meta
+                        avatar={<Avatar src={fire} />}
+                        title={<h4 style={{color : '#1A2E33 ',fontFamily: 'Avenir LT Std 55 Roman'}}>{item.title}</h4>}
+                        description={<h2 style={{color : '#1A2E33 ',fontFamily: 'Avenir LT Std 55 Roman'}}>{item.description}</h2>}
+                        />
+                    </List.Item>
+                    )}
+                />
+            </Card>
+           
+        )
+
+    }
+}
+
+export default Announcements;
---
+++ live-site/src/components/Leaderboard.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+import { Card, Divider, Row } from 'antd';
+
+import redLogo from '../assets/RedTeamLogo.png'
+import greenLogo from '../assets/GreenTeamLogo.png'
+import blueLogo from '../assets/BlueTeamLogo.png'
+import leaderboardLogo from '../assets/leaderboard.svg'
+import Team from './Team.js'
+
+const { Meta } = Card;
+
+class Leaderboard extends Component{
+
+    constructor(){
+
+        super();
+        this.state = {
+            bluePoints: 0,
+            redPoints: 0,
+            greenPoints: 0
+        }
+        
+    }
+    componentDidMount(){
+
+        this.setState({
+            bluePoints: 1000,
+            redPoints: 200,
+            greenPoints: 10
+        })
+
+    }
+
+    render(){
+        return(
+
+            <Card bordered={true} style={{ backgroundColor:'white', borderRadius:'15px', marginTop:'10%', height:'90%'}}>
+                <Meta
+                    title={<div><img src={leaderboardLogo} style={{height:'18%',width:'18%', float:'left'}}/><h1 style={{color:"#1A2E33", paddingLeft:'35%',fontFamily: 'Aleo'}}><b>Leaderboard</b></h1></div>}
+                />
+                {/* <Divider style={{marginBottom:'5%',marginTop:'1%'}}/> */}
+                <Divider/>
+
+                <div>
+                    <Row>
+                        <Team logo={blueLogo} team="Blue" points={this.state.bluePoints} outerLeftColor="#FFDD29" outerRightColor="#FDB040" innerColor="#FFF0CE"/>
+                    </Row>
+                    <Row>
+                        <Team logo={redLogo} team="Red" points={this.state.redPoints} outerLeftColor="#F0EFF5" outerRightColor="#8D9093" innerColor="#F4F4F4"/>
+                    </Row>
+                    <Row>
+                        <Team logo={greenLogo} team="Green" points={this.state.greenPoints} outerLeftColor="#FFA15C" outerRightColor="#925401" innerColor="#E5D4C0"/>
+                    </Row>
+                </div>
+                
+            </Card>
+           
+        )
+    }
+
+}
+
+export default Leaderboard;
