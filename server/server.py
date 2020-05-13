b73b7bc81970357d8e2d0b4e7b8a08c9d0ca1ec3
Evan McIntire <mcintire.evan@gmail.com>
2020-05-13 15:23:55 -0400

    Fix some server stuff

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-import json
-import hashlib
-import requests
+from functools import wraps
+
+# import json
+# import hashlib
+# import requests
 
 app = Flask(__name__)
 
@@ -11,12 +13,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 
-#### Models
+# Models
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
+
 
 class Announcement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,22 +28,49 @@ class Announcement(db.Model):
     text = db.Column(db.Text, nullable=True)
     time = db.Column(db.Integer, nullable=False)
 
-#### Routes
+# Decorators
+
+# Checks for hacker login token
+
+
+def needs_client_auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # TODO: actually do
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Checks for admin api token
+
+
+def needs_admin_auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # TODO: actually do
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Routes
+
 
 @app.route('/')
 def root():
     return 'Api Root'
 
-## Login
+# Login
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     if ('email' not in data) or ('password' not in data):
         return "Invalid body", 400
 
-    #r = requests.post(url="https://my.bit.camp/api/auth/sign_in", data=request.json)
-    #return (r.content.data, r.status_code, {"uid": r.headers['uid'], "access-token": r.headers['access-token'], "client": r.headers['client'], "Content-Type": "application/json"})
+    # r = requests.post(url="https://my.bit.camp/api/auth/sign_in",
+    # data=request.json)
+    # return (r.content.data, r.status_code, {"uid": r.headers['uid'],
+    # "access-token": r.headers['access-token'], "client": r.headers['client'],
+    # "Content-Type": "application/json"})
     return jsonify([])
+
 
 @app.route('/login/admin', methods=['POST'])
 def login_admin():
@@ -50,12 +81,16 @@ def login_admin():
     return jsonify([])
 
 
-## Announce
-@app.route('/announce', methods=['GET', 'POST', 'PUT', 'DELETE'])
+# Announce
+@app.route('/announce', methods=['GET'])
 def announce():
-    if request.method == "GET":
-        return jsonify([])
-    elif request.methid == "POST":
+    return jsonify([])
+
+
+@app.route('/announce', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@needs_admin_auth
+def admin_announce():
+    if request.methid == "POST":
         data = request.get_json()
         if ('title' not in data) or ('body' not in data):
             return "Invalid body", 400
@@ -70,78 +105,96 @@ def announce():
             return "Invalid params", 400
     return jsonify([])
 
-@app.route('/announce/subscribe')
+
+@app.route('/announce/subscribe', methods=['POST'])
+@needs_client_auth
 def announce_subscribe():
     data = request.get_json()
     if 'token' not in data:
         return "Invalid body", 400
     return jsonify([])
 
-## Entry
+# Entry
 @app.route('/entry/checkin', methods=['POST'])
+@needs_admin_auth
 def entry_checkin():
     data = request.get_json()
     if 'email' not in data:
         return "Invalid body", 400
     return jsonify([])
 
+
 @app.route('/entry/nfc-pair', methods=['POST'])
+@needs_admin_auth
 def entry_nfc():
     data = request.get_json()
     if ('email' not in data) or ('nfc' not in data):
         return "Invalid body", 400
     return jsonify([])
 
-## Events
+# Events
 @app.route('/events')
 def get_events():
     return jsonify([])
 
+
 @app.route('/events/hash')
-def get_events():
+def get_events_hash():
     return ""
 
+
 @app.route('/events/follow', methods=['POST'])
+@needs_client_auth
 def follow_event():
     return jsonify([])
 
+
 @app.route('/events/unfollow', methods=['POST'])
-def follow_event():
+@needs_client_auth
+def unfollow_event():
     return jsonify([])
+
 
 @app.route('/events/following-count')
 def get_events_count():
     return jsonify([])
 
+
 @app.route('/events/following')
+@needs_client_auth
 def get_events_following():
     return jsonify([])
 
+
 @app.route('/events/checkin', methods=['POST'])
+@needs_client_auth
 def event_checkin():
     return jsonify([])
 
-## Mentorship
+# Mentorship
 @app.route('/mentorship')
 def get_questions():
     return jsonify([])
 
+
 @app.route('/mentorship/submit', methods=['POST'])
+@needs_client_auth
 def post_question():
     return jsonify([])
+
 
 @app.route('/mentorship/claim', methods=['POST'])
 def claim_question():
     return jsonify([])
 
-## Teams
+# Teams
 @app.route('/teams')
-def get_questions():
+def get_teams():
     return jsonify([])
 
-## Get token and email
-#@app.route('/announcements/subscribe', methods=['POST'])
-#def postToken():
+# Get token and email
+# @app.route('/announcements/subscribe', methods=['POST'])
+# def postToken():
 #    data = request.get_json()
 #    token = data['token']
 #    email = data['email']
@@ -150,14 +203,21 @@ def get_questions():
 #    db.session.commit()
 #    return jsonify({'added' : email})
 #
-#def getAllTokens():
+# def getAllTokens():
 #    tokens = []
 #    for u in User.query.with_entities(User.token).all():
 #        tokens.append(list(u)[0])
 #    return tokens
 #
-#def getToken(email):
+# def getToken(email):
 #    return User.query.filter_by(email=email).first().token
+
+# Testing - simulate actions that should be handled by Dashboard
+
+
+def test_login(email, password):
+    return
+
 
 if __name__ == '__main__':
     db.create_all()
